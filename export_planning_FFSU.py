89e6c7dcb2d5aed196b82d11ee4b220e5399e0b5
Patalex123 <125601571+Patalex123@users.noreply.github.com>
2024-10-23 15:19:34 +0200

    Automatisation tous les jours
    J'ai modifié pour que la boucle s'effectue tous les jours et qu'on compare les fichiers.

--- export_planning_FFSU.py
+++ export_planning_FFSU.py
@@ -1,41 +1,43 @@
+"""
+Fonction permettant d'aller faire une requete POST pour exporter le fichier excel planning sur le site de la FFSU 
+"""
+
 # URL du fichier à télécharger
 import requests
 import pandas as pd 
-import hashlib
+import time
                       
 # Define the URL and the data you want to send with the POST request
 url = 'https://sportco.abyss-clients.com/rencontres/planning/export'
+df_original = pd.DataFrame([2,4,6])    # mettre un dataframe random pour télécharger le fichier au début
 
-# Make the POST request
-response = requests.post(url)
+while True :
+    # Make the POST request
+    response = requests.post(url)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Save the content of the response to a file
-    with open('export_planning.xlsx', 'wb') as file:
-        file.write(response.content)
-    print("File downloaded successfully!")
-else:
-    print(f"Failed to download file. Status code: {response.status_code}")
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Save the content of the response to a file
+        with open('export_planning.xlsx', 'wb') as file:
+            file.write(response.content)
+        print("File downloaded successfully!")
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
 
-# File to check
-file_name = 'export_planning.xlsx' 
+    # File to check
+    file_name = 'export_planning.xlsx' 
+    df_new = pd.DataFrame(pd.read_excel(file_name))
 
-# Open,close, read file and calculate MD5 on its contents 
-with open(file_name, 'rb') as file_to_check:
-    # read contents of the file
-    data = file_to_check.read()    
-    md5_returned = hashlib.md5(data).hexdigest
+    # compare les deux dataframes
+    if df_original.equals(df_new):
+        print("Les fichiers sont identiques.")
+    else:
+        print("Les fichiers sont différents.")
+        df_original = df_new
 
-original_md5 = '6e2a75253d379a3e7583d86c140e9286'    # mettre l'ancien hash du fichier
-
-# Finally compare original MD5 with freshly calculated and convert it to a dataframes
-if original_md5 == md5_returned:
-    print("MD5 verified.")
-else:
-    print("MD5 verification failed!.")
-    original_md5 = md5_returned
-    dfPlanning = pd.DataFrame(pd.read_excel(file_name))
-    print(dfPlanning)
+    #stoppe la boucle pendant
+    time_to_wait = 86400 #secondes
+    time.sleep(time_to_wait)
+    
 
     
