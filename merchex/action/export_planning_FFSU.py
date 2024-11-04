2d292ca16cdc6406ed0b95e41d5e69bc4ef23b47
Patalex123 <125601571+Patalex123@users.noreply.github.com>
2024-11-04 23:00:38 +0100

    changement de export_planning en une fonction toute simple

--- merchex/action/export_planning_FFSU.py
+++ merchex/action/export_planning_FFSU.py
@@ -1,32 +1,28 @@
-"""
-Fonction permettant d'aller faire une requete POST pour exporter le fichier excel planning sur le site de la FFSU 
-"""
-
-# URL du fichier à télécharger
-import requests
-import pandas as pd 
-import time
-                      
+#import pandas as pd
 # Define the URL and the data you want to send with the POST request
-url = 'https://sportco.abyss-clients.com/rencontres/planning/export'
-df_original = pd.DataFrame([2,4,6])    # mettre un dataframe random pour télécharger le fichier au début
+#url = 'https://sportco.abyss-clients.com/rencontres/planning/export'
+#df_original = pd.DataFrame([2,4,6])    # mettre un dataframe random pour télécharger le fichier au début
 
-while True :
-    # Make the POST request
+def export_excel_website(url : str,df_original, name_file : str):
+    """
+    Fonction permettant d'aller faire une requete POST pour exporter le fichier excel planning sur le site de la FFSU 
+    """
+    import requests
+    import pandas as pd 
+
     response = requests.post(url)
 
     # Check if the request was successful
     if response.status_code == 200:
         # Save the content of the response to a file
-        with open('export_planning.xlsx', 'wb') as file:
+        with open(name_file, 'wb') as file:
             file.write(response.content)
         print("File downloaded successfully!")
     else:
         print(f"Failed to download file. Status code: {response.status_code}")
 
     # File to check
-    file_name = 'export_planning.xlsx' 
-    df_new = pd.DataFrame(pd.read_excel(file_name))
+    df_new = pd.DataFrame(pd.read_excel(name_file))
 
     # compare les deux dataframes
     if df_original.equals(df_new):
@@ -34,10 +30,4 @@ while True :
     else:
         print("Les fichiers sont différents.")
         df_original = df_new
-
-    #stoppe la boucle pendant
-    time_to_wait = 86400 #secondes
-    time.sleep(time_to_wait)
-    
-
     
