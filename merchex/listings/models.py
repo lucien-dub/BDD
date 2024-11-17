92bbbd8e77631f02080750dac589b10be3245791
lucien-dub <lucien.rozwa@hotmail.fr>
2024-11-17 23:37:26 +0100

    supprime les models

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,38 +1,3 @@
 # listings/models.py
 
-from django.db import models
-
-
-class Equipes(models.Model):
-
-    class Sport(models.TextChoices):
-        Rugby = 'Rugby'
-        Football = 'Football'
-        Volleyball = 'Volleyball'
-        Handball = 'Handball'
-        Basketball = 'Basketball'
-        Tennis = 'Tennis'
-
-
-    id = models.AutoField(primary_key=True)
-    sport = models.fields.CharField(choices = Sport.choices, max_length = 100)
-    name = models.fields.CharField(max_length=100)
-    lieu = models.fields.CharField(max_length=100)
-    niveau = models.fields.IntegerField()
-
-
-class Matchs(models.Model):
-    date = models.fields.DateField()
-    equipe1 = models.ForeignKey(Equipes,related_name='equipe1', on_delete = models.PROTECT)
-    equipe2 = models.ForeignKey(Equipes, related_name='equipe2', on_delete = models.PROTECT)
-    sport = {"Rugby","Tennis","Football","Basketball","Handball","Volleyball"}
-    passe = models.BooleanField()
-
-
-class MyManager(models.Manager):
-    use_in_migrations = True
-
-class MyModel(models.Model):
-    objects = MyManager()
-
 
