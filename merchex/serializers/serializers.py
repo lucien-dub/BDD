5d07e7b8f8197fd3ff6dd490f65225b568d0d18c
lucien-dub <lucien.rozwa@hotmail.fr>
2024-12-24 15:39:09 +0100

    serializers heure

--- merchex/serializers/serializers.py
+++ merchex/serializers/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 from creation_bdd.creation_bdd import Match
+from db.models import 
  
 class MatchSerializer(ModelSerializer):
  
     class Meta:
         model = Match
-        fields = ['sport', 'date', 'equipe1', 'equipe2', 'score1', 'score2']
-        
+        fields = ['sport', 'date', 'equipe1', 'equipe2', 'score1', 'score2', 'heure']
