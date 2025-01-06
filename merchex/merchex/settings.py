4340937dc46a52dd6a3f53ea5f16f3d4f0f3ada1
Patalex123 <125601571+Patalex123@users.noreply.github.com>
2025-01-06 14:19:43 +0100

    Affichage des matchs

--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -93,6 +93,8 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8100",
     "http://127.0.0.1:8100",
