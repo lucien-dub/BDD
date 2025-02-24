698d50f50d586c984d88046f6a40ace08990a836
Patalex123 <125601571+Patalex123@users.noreply.github.com>
2025-02-24 18:53:56 +0100

    ajout de quelques bails

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -186,6 +186,15 @@ class CotesAPIView(APIView):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
 
 class UsersPointsAPIView(APIView):
 
