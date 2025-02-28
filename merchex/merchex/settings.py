e36662029e5dd0653b96077884e56c00258e862e
lucien-dub <lucien.rozwa@hotmail.fr>
2025-02-28 17:34:07 +0100

    enlever log

--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -136,26 +136,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'merchex.wsgi.application'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
