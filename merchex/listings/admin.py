4545ed65723be94ed686f1253914b51aa1419170
Patalex123 <125601571+Patalex123@users.noreply.github.com>
2025-04-24 17:48:58 +0200

    Update admin.py

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -4,11 +4,14 @@ from background.actualisation_bdd import Match
 from django.contrib.auth.models import User
 from listings.models import UserPoints
 from listings.models import Cote, Pari, photo_profil
+from listings.models import Verification, EmailVerificationToken
 
 admin.site.register(Match)
 admin.site.register(UserPoints)
 admin.site.register(Cote)
 admin.site.register(Pari)
 admin.site.register(photo_profil)
+admin.site.register(Verification)
+admin.site.register(EmailVerificationToken)
 # Register your models here.
 
